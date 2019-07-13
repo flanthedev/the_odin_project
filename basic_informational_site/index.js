78ab84c1dbc12230bfd6370578b3dd91c8032772
flannerychristopher <flannerychristopher@gmail.com>
2019-07-13 14:38:47 -0400

    npm init and install express

--- basic_informational_site/index.js
+++ basic_informational_site/index.js
@@ -1,20 +1,27 @@
+var express = require('express');
 var fs = require('fs');
 http = require('http');
 var dt = require('./datetime');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+// const hostname = '127.0.0.1';
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  fs.readFile('index.html', (err, data) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-  // res.write("The date and time are currently: " + dt.myDateTime());
-    res.write(data);
-    res.end();
-  })
-});
+// const server = http.createServer((req, res) => {
+//   fs.readFile('index.html', (err, data) => {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//   // res.write("The date and time are currently: " + dt.myDateTime());
+//     res.write(data);
+//     res.end();
+//   })
+// });
 
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+var app = express()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
