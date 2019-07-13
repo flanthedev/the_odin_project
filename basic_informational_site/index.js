fa13687a5d4f9d694c0c2bc2682e83d050650852
flannerychristopher <flannerychristopher@gmail.com>
2019-07-13 14:23:54 -0400

    show the time at root

---
+++ basic_informational_site/index.js
@@ -0,0 +1,23 @@
+http = require('http');
+var dt = require('./datetime');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end();
+});
+
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write("The date and time are currently: " + dt.myDateTime());
+//   res.end();
+// }).listen(8080);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
