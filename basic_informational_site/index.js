086dee127448a0ebed7abf33a9d0ec553922f2e9
flannerychristopher <flannerychristopher@gmail.com>
2019-07-13 14:36:01 -0400

    render index.html

--- basic_informational_site/index.js
+++ basic_informational_site/index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 http = require('http');
 var dt = require('./datetime');
 
@@ -5,18 +6,14 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
+  fs.readFile('index.html', (err, data) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.write("The date and time are currently: " + dt.myDateTime());
+    res.write(data);
+    res.end();
+  })
 });
 
-
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/html'});
-//   res.write("The date and time are currently: " + dt.myDateTime());
-//   res.end();
-// }).listen(8080);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
